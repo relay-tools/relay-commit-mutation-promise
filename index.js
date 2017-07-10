ef21bcc69962fd790b595a2121ca2bff895c40bc
Rob Richard <rob@1stdibs.com>
2017-07-09 20:31:17 -0400

    take full mutation config

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 var Relay = require('react-relay');
 
-function commitMutationPromise(environment, mutation, variables) {
+function commitMutationPromise(environment, config) {
     return new Promise(function (resolve, reject) {
         Relay.commitMutation(
             environment,
-            {
-                mutation,
-                variables,
-                onCompleted: function (payload, errors) {
-                    if (errors) {
-                        reject(errors[0]);
-                        return;
+            Object.assign(
+                {},
+                config,
+                {
+                    onCompleted: function (payload, errors) {
+                        if (errors) {
+                            reject(errors[0]);
+                            return;
+                        }
+                        resolve(payload);
+                    },
+                    onError: function (error) {
+                        reject(error);
                     }
-                    resolve(payload);
-                },
-                onError: function (error) {
-                    reject(error);
                 }
-            }
+            )
         );
     });
 }
