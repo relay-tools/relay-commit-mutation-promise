4ba1934761ab1dd85448837cd664f7b5381d0c25
Rob Richard <rob@1stdibs.com>
2017-07-09 20:27:31 -0400

    add code

---
+++ compat.js
@@ -0,0 +1,25 @@
+var Relay = require('react-relay/compat');
+
+function commitMutationPromise(environment, mutation, variables) {
+    return new Promise(function (resolve, reject) {
+        Relay.commitMutation(
+            environment,
+            {
+                mutation,
+                variables,
+                onCompleted: function (payload, errors) {
+                    if (errors) {
+                        reject(errors[0]);
+                        return;
+                    }
+                    resolve(payload);
+                },
+                onError: function (error) {
+                    reject(error);
+                }
+            }
+        );
+    });
+}
+
+module.exports = commitMutationPromise;
