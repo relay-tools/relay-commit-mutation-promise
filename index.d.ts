661f525d995ddd43e7a52da07fcf4e95f686a00c
Jared Kass <jared.d.kass@gmail.com>
2019-10-09 10:58:34 -0400

    fix: fix typing (#3)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'relay-commit-mutation-promise' {
-    import { MutationConfig, OperationType, Environment } from 'relay-runtime';
+    import { MutationConfig, MutationParameters, Environment } from 'relay-runtime';
 
-    export default function commitMutation<T extends OperationType>(environment: Environment, config: Omit<MutationConfig<T>, 'onError' | 'onCompleted'>) : Promise<T['response']>
+    export default function commitMutation<T extends MutationParameters>(environment: Environment, config: Omit<MutationConfig<T>, 'onError' | 'onCompleted'>) : Promise<T['response']>
 }
