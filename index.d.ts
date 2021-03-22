6a1ce099627c748d8b05d8b3f8a1e429b4f97ef8
Rob Richard <rob@1stdibs.com>
2021-03-22 14:16:44 -0400

    use environment interface in types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'relay-commit-mutation-promise' {
-    import { MutationConfig, MutationParameters, Environment } from 'relay-runtime';
+    import { MutationConfig, MutationParameters, IEnvironment } from 'relay-runtime';
 
-    export default function commitMutation<T extends MutationParameters>(environment: Environment, config: Omit<MutationConfig<T>, 'onError' | 'onCompleted'>) : Promise<T['response']>
+    export default function commitMutation<T extends MutationParameters>(environment: IEnvironment, config: Omit<MutationConfig<T>, 'onError' | 'onCompleted'>) : Promise<T['response']>
 }
